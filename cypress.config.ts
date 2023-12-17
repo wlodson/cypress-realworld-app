6eacb445e76fa105293bb96cd2b4e3e6d781ff6f
Daniel Włodarczyk <dwo@spyro-soft.com>
2023-12-17 15:10:57 +0100

    cypress-cloud-test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: "idamud",
+  projectId: "pnqzsi",
   viewportHeight: 1000,
   viewportWidth: 1280,
   retries: {
